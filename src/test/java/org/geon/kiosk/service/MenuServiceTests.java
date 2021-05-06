d753c8e2f5e079724337a40cda7f49a066620f0e
parkgun6 <76540460+parkgun6@users.noreply.github.com>
2021-05-06 20:30:20 +0900

    MenuService Tests

---
+++ src/test/java/org/geon/kiosk/service/MenuServiceTests.java
@@ -0,0 +1,32 @@
+package org.geon.kiosk.service;
+
+import lombok.extern.log4j.Log4j2;
+import org.geon.kiosk.dto.MenuDTO;
+import org.geon.kiosk.dto.PageRequestDTO;
+import org.geon.kiosk.dto.PageResultDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+@SpringBootTest
+@Log4j2
+public class MenuServiceTests {
+
+    @Autowired
+    MenuService menuService;
+
+    @Test
+    public void testGetListPage() {
+
+        PageRequestDTO pageRequestDTO = new PageRequestDTO();
+
+        PageResultDTO<MenuDTO, Object[]> result = menuService.getListPage(1L,pageRequestDTO);
+
+        result.getDtoList().forEach(menuDTO -> {
+            log.info(menuDTO);
+        });
+    }
+
+}
