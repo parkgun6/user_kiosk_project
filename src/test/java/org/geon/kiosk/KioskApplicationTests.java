df1bd70bd413fd5b623153ecb898508695e8cfbd
parkgun6 <76540460+parkgun6@users.noreply.github.com>
2021-05-06 20:30:49 +0900

    Kiosk Tests

---
+++ src/test/java/org/geon/kiosk/KioskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.geon.kiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KioskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
