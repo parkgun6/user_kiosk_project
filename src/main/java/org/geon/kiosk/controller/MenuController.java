c734df014808be139482841a8584c7dcbe8632c6
parkgun6 <76540460+parkgun6@users.noreply.github.com>
2021-05-10 19:25:34 +0900

    MenuController

--- src/main/java/org/geon/kiosk/controller/MenuController.java
+++ src/main/java/org/geon/kiosk/controller/MenuController.java
@@ -2,13 +2,17 @@ package org.geon.kiosk.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.geon.kiosk.dto.MenuDTO;
 import org.geon.kiosk.dto.PageRequestDTO;
 import org.geon.kiosk.service.MenuService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @Log4j2
@@ -26,6 +30,24 @@ public class MenuController {
         model.addAttribute("result", service.getListPage(sno, pageRequestDTO));
 
         return "/kiosk/order";
+    }
+
+    @GetMapping("/payment")
+    public void payment(){
+
+        log.info("main---------------------------------------");
+
+    }
+
+    @GetMapping("/{mno}/menu")
+    @ResponseBody
+    public ResponseEntity<MenuDTO> getMenu(@PathVariable Long mno){
+
+        log.info("list---------------------------------------");
+
+        MenuDTO menuDTO =  service.getMenu(mno);
+
+        return new ResponseEntity<>(menuDTO, HttpStatus.OK);
     }
 
     @GetMapping("/main")
